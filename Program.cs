853d555904bbc5c82b17529be08f9e68dea32818
agiknightt <70384293+agiknightt@users.noreply.github.com>
2020-09-05 01:02:38 +0700

    Initial commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+
+namespace Task_20
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            float health = 5, maxHealth = 100;
+            bool enterOrExit = true;           
+
+            while (enterOrExit)
+            {
+                Console.Write("Введите процент заполнения шкалы : ");
+                health = Convert.ToSingle(Console.ReadLine());
+
+                DrawBar(health, maxHealth, 0);
+
+                Console.ReadKey();
+                Console.Clear();
+            }            
+        }
+
+        static void DrawBar(float value, float maxValue,  int position, char symbol = '#',  char emptySymbol = '_')
+        {
+            string bar = "";
+
+            Console.SetCursorPosition(0, position);
+            Console.Write('[');
+
+            value *= maxValue / 100;
+
+            for (int i = 0; i < value; i++)
+            {
+                bar += symbol;
+            }
+
+            for (float i = value; i < maxValue; i++)
+            {
+                bar += emptySymbol;
+            }
+            Console.Write(bar + ']');
+        }
+    }
+}
